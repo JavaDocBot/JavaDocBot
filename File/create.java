c34993f0e070524eb1b5fa71619334ed5ff5816f
JavaDocBot <162185914+JavaDocBot@users.noreply.github.com>
2024-03-04 22:00:04 +0300

    Create create.java

---
+++ File/create.java
@@ -0,0 +1,26 @@
+import java.io.File;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            // Создание файла Test.txt
+            File file = new File("Test.txt");
+            if (file.createNewFile()) {
+                System.out.println("Файл Test.txt создан");
+            } else {
+                System.err.println("Файл Test.txt уже существует");
+            }
+
+            // Переименование файла Test.txt в Test1.txt
+            File newFile = new File("Test1.txt");
+            if (file.renameTo(newFile)) {
+                System.out.println("Файл Test.txt переименован в Test1.txt");
+            } else {
+                System.err.println("Не удалось переименовать файл Test.txt");
+            }
+        } catch (IOException e) {
+            System.err.println("Произошла ошибка ввода/вывода: " + e.getMessage());
+        }
+    }
+}
