a15621cb9e33b5a1d370e20e672cb335c7e9ee5b
JavaDocBot <162185914+JavaDocBot@users.noreply.github.com>
2024-03-09 13:32:30 +0300

    Create variable_name.java

---
+++ Variable/variable_name.java
@@ -0,0 +1,24 @@
+/*
+Имя переменной должно начинаться с буквы и представлять собой сочетание букв и цифр.
+Буквами считаются символы `'A' - 'Z'`, `'a' - 'z'`, `'_'`, `'$'` и любой другой символ кодировки в Юникод,
+соответствующий букве. Аналогично цифрами считаются как обычные десятичные цифры `'0' - '9'`, так и любые
+символы кодировки в Юникод, использующиеся для обозначения цифр в конкретном языке. Символы вроде `'+'`
+или пробел нельзя использовать в качестве имен переменных. Все символы в имени переменной важны,
+причем _регистр букв_ также учитывается. Длина имени переменной не ограничивается.
+
+В качестве имени переменной нельзя использовать _зарезервированные_ слова Java, указанные в Таблице:
+
+|-------------|------------|--------------|----------------|--------------|------------|
+| abstract    |   assert   |   boolean    |   break        |   byte       |   case     |
+| catch       |   char     |   class      |   const        |   continue   |   default  |
+| do          |   double   |   else       |   enum         |   exports    |   extends  |
+| final       |   finally  |   float      |   for          |   goto       |   if       |
+| implements  |   import   |   instanceof |   int          |   interface  |   long     |
+| module      |   native   |   new        |   new          |   non-sealed |   opens    |
+| package     |   permits  |   private    |   protected    |   provides   |   public   |
+| record      |   requires |   return     |   sealed       |   short      |   static   |
+| strictfp    |   super    |   switch     |   synchronized |   this       |   throw    |
+| throws      |   to       |   transient  |   transitive   |   try        |   uses     |
+| var         |   void     |   volatile   |   while        |   with       |   yield    |
+
+*/
